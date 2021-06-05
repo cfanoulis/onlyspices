1b83bc0872288a0d6850b97939d0bb3ad5b72131
Charalampos Fanoulis <yo@fanoulis.dev>
2021-06-05 23:56:40 +0300

    feat: use props for spice card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,19 @@
-const Card = () => {
+import { format } from "date-fns";
+
+interface CardProps {
+	spice: string
+	purchasedAt: number
+	tag: number
+}
+
+const Card = (props: CardProps) => {
+	const date = format(props.purchasedAt ?? 0, 'yyyy-mm-dd')
 	return (
 		<div className="border-gray-300 border-solid border-2 rounded-xl shadow-xl p-8 m-8 flex flex-row">
 			<div>
-				<p className="font-bold">Tumeric</p>
-				<p className="font-mono">2021-01-01</p>
-				<p className="font-mono text-xs">OS-001</p>
+				<p className="font-bold">{props.spice ?? 'Tumeric'}</p>
+				<p className="font-mono">{date}</p>
+				<p className="font-mono text-xs">OS-{(props.tag ?? 'NOTAG').toString().padStart(3, '0')}</p>
 			</div>
 			<div className="flex flex-col justify-between ml-16">
 				<button className="rounded-lg bg-blue-400 p-1 w-20 text-sm font-semibold">Depleted</button>{' '}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,22 +8,9 @@ const IndexPage = () => (
 		</div>
 		{/*cards and stuff*/}
 		<div className="flex flex-row p-8 items-center flex-wrap justify-center">
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
+			<Card spice='Cajun pepper' purchasedAt={1622926497} tag={1}/>
+			<Card spice='Salt' purchasedAt={1622926497} tag={2}/>
+			<Card spice='Cumin' purchasedAt={1622926497} tag={3}/>
 			<Card />
 		</div>
 	</div>
